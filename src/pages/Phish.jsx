df7a84fd5638f746ea9d886f3ba47551b2fd5149
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-25 22:18:08 -0400

    feth from api. displaying name

--- src/pages/Phish.jsx
+++ src/pages/Phish.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PhishPic from '../images/phish.jpeg'
 import BandComponent from '../components/BandComponent'
+import axios from 'axios'
 
 const Phish = () => {
+  const [phish, setPhish] = useState({})
+  async function fetchData() {
+    const resp = await fetch(
+      'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=phish&api_key=15a8aa9b0d09e6572aa2b2f6ab29fdb0&format=json'
+    )
+    const data = await resp.json()
+    setPhish(data.artist)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  console.log(phish.bio)
   return (
     <>
       <BandComponent
-        name="Phish"
-        pic={PhishPic}
-        description="Phish is an American rock band that formed in Burlington, Vermont in
-            1983. The band is known for musical improvisation, extended jams,
-            blending of genres, and a dedicated fan base. The band consists of
-            guitarist Trey Anastasio, bassist Mike Gordon, drummer Jon Fishman,
-            and keyboardist Page McConnell, all of whom perform vocals, with
-            Anastasio being the primary lead vocalist."
+        name={phish.name}
+        // pic="https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png"
+        // description={phish.bio.links.summary}
         albumTitle="Big Boat"
         year="2016"
         songs="13"
